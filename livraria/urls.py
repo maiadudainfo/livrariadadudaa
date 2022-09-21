7dad19fca9889ef93834227d8c89cb2f0acce3e7
Maria Eduarda Alves da Maia <duddaa.maia@gmail.com>
2022-09-21 20:17:45 -0300

    commit api

--- livraria/urls.py
+++ livraria/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from core.views import CategoriaViewSet, EditoraViewSet, LivroViewSet, AutorViewSet
+
+router = DefaultRouter()
+router.register(r"categorias", CategoriaViewSet)
+router.register(r"editoras", EditoraViewSet)
+router.register(r"livros", LivroViewSet)
+router.register(r"autores", AutorViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include(router.urls)),
 ]

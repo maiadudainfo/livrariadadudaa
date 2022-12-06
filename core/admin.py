427cf10099f338f1e5bba7839fef6501b48a836b
maiadudainfo <duddaa.maia@gmail.com>
2022-12-06 16:08:01 -0300

    Aula 24 Configurando variáveis de ambiente

--- core/admin.py
+++ core/admin.py
@@ -4,15 +4,23 @@ from django.utils.translation import gettext_lazy as _
 
 from core.models import Autor, Categoria, Editora, Livro, Usuario
 
-admin.site.register(Autor)
-admin.site.register(Categoria)
-admin.site.register(Editora)
-admin.site.register(Livro)
 
 class UsuarioAdmin(UserAdmin):
     fieldsets = (
         (None, {"fields": ("username", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name", "email", "cpf", "telefone", "data_nascimento")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "cpf",
+                    "telefone",
+                    "data_nascimento",
+                )
+            },
+        ),
         (
             _("Permissions"),
             {
@@ -28,4 +36,48 @@ class UsuarioAdmin(UserAdmin):
         (_("Important dates"), {"fields": ("last_login", "date_joined")}),
     )
 
-admin.site.register(Usuario, UsuarioAdmin)
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = (
+        "nome",
+        "email",
+    )
+    ordering = ("nome",)
+    search_fields = ("nome", "email")
+
+
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ("descricao",)
+    search_fields = ("descricao",)
+    list_filter = ("descricao",)
+    ordering = ("descricao",)
+
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ("nome",)
+    search_fields = ("nome",)
+    list_filter = ("nome",)
+    ordering = ("nome",)
+
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = (
+        "categoria",
+        "titulo",
+        "editora",
+    )
+    ordering = ("categoria", "titulo")
+    search_fields = (
+        "categoria__descricao",
+        "titulo",
+        "editora__nome",
+    )
+    list_filter = (
+        "categoria",
+        "editora",
+    )
+    list_per_page = 15
--- livraria/settings.py
+++ livraria/settings.py
@@ -1,5 +1,14 @@
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+environ.Env.read_env((os.path.join(BASE_DIR, '.env')))
+
+SECRET_KEY = env('SECRET_KEY')
+DEBUG = env('DEBUG')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
+DATABASES = {'default': env.db()}
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 

ffb452d9a35c2b273a0a468feaf90004691e627d
maiadudainfo <duddaa.maia@gmail.com>
2022-12-13 13:10:06 -0300

    settings no autentication

--- livraria/urls.py
+++ livraria/urls.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.db import router
 from django.urls import include, path
+
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -10,39 +12,35 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from uploader.router import router as uploader_router
 
 from core.views import AutorViewSet, CategoriaViewSet, EditoraViewSet, LivroViewSet
-from media.router import router as media_router
+from uploader.router import router as uploader_router
 
 router = DefaultRouter()
-router.register(r'autores', AutorViewSet)
-router.register(r'categorias', CategoriaViewSet)
-router.register(r'editoras', EditoraViewSet)
-router.register(r'livros', LivroViewSet)
+router.register(r'autores',AutorViewSet)
+router.register(r'categorias',CategoriaViewSet)
+router.register(r'editoras',EditoraViewSet)
+router.register(r'livros',LivroViewSet)
+
 
 urlpatterns = [
-    # Simple JWT
+    path("api/", include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('', include (router.urls)),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # Admin
-    path('admin/', admin.site.urls),
-    # Upload de imagens e documentos
-    path('api/media/', include(media_router.urls)),
-    # OpenAPI 3
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("api/media/", include(uploader_router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        'api/swagger/',
-        SpectacularSwaggerView.as_view(url_name='schema'),
-        name='swagger-ui',
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
     ),
     path(
-        'api/redoc/',
-        SpectacularRedocView.as_view(url_name='schema'),
-        name='redoc',
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
-    # API REST
-    path('api/', include(router.urls)),
     path("api/media/", include(uploader_router.urls)),
 ]
 
